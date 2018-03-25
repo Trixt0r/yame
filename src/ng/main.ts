6d50efa4454226273b9c23e8a9f501ca1592898b
Trixt0r <h.reich90@gmail.com>
2018-03-25 01:22:58 +0100

    Entry point cleanup

--- src/ng/main.ts
+++ src/ng/main.ts
@@ -3,16 +3,10 @@ import { extend } from 'common/require';
 // Define a way to require yame
 extend(yame);
 
-import { ipcRenderer } from 'electron'; // satisfy the tsc when compiling with webpack
-import * as _ from 'lodash';
-import * as path from 'path';
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { enableProdMode } from '@angular/core';
 import { environment } from '../environments';
-import { Plugin } from 'common/interface/plugin';
-import { Environment } from './environment';
 import { PluginManager } from './module/plugin/manager';
 
 if (environment.production)
